ee3ec8a40f5f90319ea9542f0e7fbe29879c52cc
David Herrmann <dh.herrmann@gmail.com>
2017-12-12 15:18:28 +0100

    c-siphash: minor coding-style and documentation fixes
    A set of minor coding-style and documentation fixes. No functional
    changes.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/c-siphash.h
+++ src/c-siphash.h
@@ -1,15 +1,15 @@
 #pragma once
 
 /**
- * Standalone SipHash24 Implementation in Standard ISO-C11
+ * Standalone SipHash Implementation in Standard ISO-C11
  *
- * This library provides a SipHash24 API, that is fully implemented in ISO-C11
+ * This library provides a SipHash API, that is fully implemented in ISO-C11
  * and has no external dependencies. The library performs no memory allocation,
  * and provides a streaming API where data to be hashed can be appended
  * piecemeal.
  *
- * A hash state is represented by the "CSipHash" structure, which should be
- * initialized with a unique seed before use.
+ * A streaming-capable hash state is represented by the "CSipHash" structure,
+ * which should be initialized with a unique seed before use.
  */
 
 #ifdef __cplusplus
@@ -22,16 +22,19 @@ extern "C" {
 typedef struct CSipHash CSipHash;
 
 /**
- * struct CSipHash - SipHash24 state object
- * @v0-@v3:     internal state
- * @padding:    pending bytes that were not a multiple of 8
- * @n_bytes:    number of hashed bytes
+ * struct CSipHash - SipHash state object
+ * @v0-@v3:             internal state
+ * @padding:            pending bytes that were not a multiple of 8
+ * @n_bytes:            number of hashed bytes
  *
  * The state of an inflight hash is represenetd by a CSipHash object. Before
  * hashing, it must be initialized with c_siphash_init(), providing a unique
  * random hash seed. Data is hashed by appending it to the state object, using
  * c_siphash_append(). Finally, the hash is read out by calling
  * c_siphash_finalize().
+ *
+ * This state object has no allocated resources. It is safe to release its
+ * backing memory without any further action.
  */
 struct CSipHash {
         uint64_t v0;
